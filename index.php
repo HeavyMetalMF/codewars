ef06f1be94f4f297167076d03991ca1a35a386bd
HeavyMetalMF <condrashpv20123@mail.ru>
2021-02-28 16:04:16 -0800

    update

--- index.php
+++ index.php
@@ -1,1 +1,33 @@
 <script src="wars.js"></script>
+<?php
+
+//descendingOrder
+//function descendingOrder(int $n) {
+//    $arr = str_split($n);
+//    $result = '';
+//    rsort($arr);
+//    for ($i = 0; $i < count($arr); $i++){
+//        $result .= (int)$arr[$i];
+//    }
+//    $result = (int)$result;
+//    return $result;
+//}
+//echo descendingOrder(456)
+
+//odd_or_even
+//function odd_or_even(array $a): string {
+//    return (array_sum($a) % 2 == 0) ? 'even' : 'odd';
+//}
+//echo odd_or_even([2, 5, 34, 6])
+
+
+function last($x){
+    $result = [];
+    $arr = explode(' ', $x);
+    $min = '';
+
+    return var_dump($arr);
+}
+echo last('man i need a taxi up to ubud');
+?>
+
